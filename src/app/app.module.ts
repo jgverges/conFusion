38510d181e4c12c9eb8918129e2a7e5aa15d7c57
jgverges <55912813+jgverges@users.noreply.github.com>
2020-08-03 18:32:25 +0200

    semana1-angular components part2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,12 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import {MatGridListModule} from '@angular/material/grid-list';
 import { MatIconModule} from '@angular/material/icon';
 import { MatListModule} from '@angular/material/list';
 import { MenuComponent } from './menu/menu.component';
+import {MatCardModule} from '@angular/material/card';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -17,6 +20,8 @@ import { MenuComponent } from './menu/menu.component';
     BrowserModule,
     BrowserAnimationsModule,
     FlexLayoutModule,
+    MatCardModule,
+    MatGridListModule,
     MatIconModule,
     MatListModule,
     MatToolbarModule
